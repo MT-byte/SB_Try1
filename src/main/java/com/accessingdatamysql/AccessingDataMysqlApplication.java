4712c8ce7d36302b9f5caf090c7ef40c6512d7be
MT-byte <m.dhakan1@nuigalway.ie>
2024-06-22 23:09:28 +0100

    committing config changes

--- src/main/java/com/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -2,9 +2,20 @@ package com.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AccessingDataMysqlApplication {
+
+    // This REST endpoint method is not required for any of the features of application,
+    // however is left here to confirm the application works when packaged as a war file.
+    @GetMapping("/hello")
+    public String hello2() {
+        return "Hello Again";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataMysqlApplication.class, args);
