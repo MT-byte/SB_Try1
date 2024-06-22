19788137e0c16660c749ac7252dcee688f1a5332
MT-byte <m.dhakan1@nuigalway.ie>
2024-06-22 19:38:02 +0100

    commit

---
+++ src/test/java/com/SBTrial/SB_App/SbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SBTrial.SB_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
